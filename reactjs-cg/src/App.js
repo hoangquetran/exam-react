b575b7370e53569d9f562d6e1a2ae10be571c676
Que Tran <hoanquexttx@gmail.com>
2022-08-16 17:38:53 +0700

    login form use bootstrap

--- reactjs-cg/src/App.js
+++ reactjs-cg/src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import './App.css';
 // import ShowInfor from './componnents/ShowInforBrowser';
 // import ShowInforStudents from './componnents/ShowInforStudents';
-import { ShowProfileCard } from './componnents/ProfileCard/ShowProfileCard';
+// import { ShowProfileCard } from './componnents/ProfileCard/ShowProfileCard';
+import { LoginForm } from './componnents/Login/Login';
 
 function App() {
   return (
     <div className="App">
       {/* <ShowInfor /> */}
       {/* <ShowInforStudents /> */}
-      <ShowProfileCard />
+      {/* <ShowProfileCard /> */}
+      <LoginForm />
     </div>
   );
 }
